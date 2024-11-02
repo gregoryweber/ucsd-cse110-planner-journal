99c6d0ca08fc36f9732da55b3a0a4fcbbf18944d
Justin <notjsouza@gmail.com>
2024-11-02 05:37:01 -0700

    Minor adjustments to Journal Entry page structure

--- client/src/components/Journal-Page.tsx
+++ client/src/components/Journal-Page.tsx
@@ -5,7 +5,11 @@ const JournalPage = () => {
   return (
     <div>
        <div>
-        <button style={{backgroundColor: "#21e4e6", color: "black"}}>Return to Planner</button>
+        <button 
+        style={{backgroundColor: "#21e4e6", color: "black"}}
+        >
+          Return to Planner
+        </button>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <h1>Journal Entry</h1>
         </div>
