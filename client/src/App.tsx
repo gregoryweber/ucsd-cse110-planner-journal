cae9e84175de3c51bf1dc3a55bb20afdbbb88340
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2024-11-07 15:36:03 -0800

    Convert file from html to tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MonthChangeButton from './Components/MonthChangeButton/monthChangeButton';
 
-function App() {
+
+const App = () =>  {
   return (
     null
   );
 }
 
 export default App;
+
+
+//function App() {
+  //return (
+    //null
+  //);
+//}
+
+//const App = () => {
+  //return (
+ 
+      //<div>
+     //<MonthChangeButton/>
+    //</div>
+  //);
+// };
