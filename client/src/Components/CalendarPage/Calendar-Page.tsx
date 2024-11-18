0c9b9ef597d911644f7ec992b598a06a57981926
CameronArch <cameronarch598@gmail.com>
2024-11-17 20:49:14 -0800

    Centered Title of Planner

--- client/src/Components/CalendarPage/Calendar-Page.tsx
+++ client/src/Components/CalendarPage/Calendar-Page.tsx
@@ -28,66 +28,82 @@ const CalendarPage: React.FC = () => {
     return (
         <>
         <div style={{
-            display: 'flex',
-            justifyContent: 'space-between',
+            display: 'grid',
+            gridTemplateColumns: '120px 1fr 120px',
             alignItems: 'center',
             padding: '2px',
+            width: '100%',
         }}>
+        <div style={{ width: '120px' }}> 
             <LogoutButton/>
+        </div>
         <h1 style={{
             textAlign: 'center',
             backgroundColor: '#D9D9D9',
-            width: '500px', 
+            width: '500px',  
             height: '40px', 
-            margin: '0 auto 16px auto', 
+            margin: '0 auto', 
             borderRadius: '10px',
             fontWeight: 'normal',
             fontStyle: 'italic',
             display: 'flex',
             alignItems: 'center', 
             justifyContent: 'center',
+            position: 'relative', 
         }}>
-                PLANNER
-            </h1>
-        </div>
-            <div style={flexContainer}>
-                <button style={{
-                    border: '0',
-                    height: '32px',
-                    marginTop: '16px',
-                    backgroundColor: '#2BE0E9',
-                    fontWeight: 'bold',
-                    width: '32px',
-                    borderRadius: '4px',
-                }}>{'<'}
-                </button>
-
-            <h1 style={{
-                textAlign: 'center',
-                backgroundColor: '#D9D9D9',
-                color: 'black',
-                fontWeight: 'normal',
-                fontSize: '24px',
-                width: '300px', 
-                height: '32px', 
-                margin: '0 16px',
-                display: 'flex',
-                alignItems: 'center', 
-                justifyContent: 'center',
+            <span style={{
+                position: 'absolute',  
+                left: '50%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)', 
+                width: 'auto',  
+                textAlign: 'center'
             }}>
-                MONTH YEAR
+                PLANNER
+            </span>
             </h1>
+            <div /> {/* Empty div to balance the space */}
+        </div>
+        <div style={flexContainer}>
+    <button style={{
+        border: '0',
+        height: '32px',
+        marginTop: '4px', // Reduced from 16px
+        backgroundColor: '#2BE0E9',
+        fontWeight: 'bold',
+        width: '32px',
+        borderRadius: '4px 0 0 4px', // Only round left corners
+        marginRight: '0', // Remove right margin
+    }}>{'<'}</button>
 
-                <button style={{
-                    border: '0',
-                    height: '32px',
-                    marginTop: '16px',
-                    backgroundColor: '#2BE0E9',
-                    fontWeight: 'bold',
-                    width: '32px',
-                    borderRadius: '4px',
-                }}>{'>'}</button>
-            </div>
+    <h1 style={{
+        textAlign: 'center',
+        backgroundColor: '#D9D9D9',
+        color: 'black',
+        fontWeight: 'normal',
+        fontSize: '24px',
+        width: '300px', 
+        height: '32px', 
+        margin: '4px 0', // Changed from '0 16px'
+        display: 'flex',
+        alignItems: 'center', 
+        justifyContent: 'center',
+    }}>
+        MONTH YEAR
+    </h1>
+
+    <button style={{
+        border: '0',
+        height: '32px',
+        marginTop: '4px', // Reduced from 16px
+        backgroundColor: '#2BE0E9',
+        fontWeight: 'bold',
+        width: '32px',
+        borderRadius: '0 4px 4px 0', // Only round right corners
+        marginLeft: '0', // Remove left margin
+    }}>{'>'}</button>
+</div>
+
             <div style={{
                 display: 'grid',
                 gridTemplateRows: 'auto repeat(5, 1fr)', // Changed to 1fr for equal sizing
