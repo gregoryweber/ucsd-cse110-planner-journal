1934ea25c0ec3ce7b66850f5062e2b7c934390ba
Justin <44349024+notjsouza@users.noreply.github.com>
2024-11-18 19:32:42 -0800

    Fixed (hopefully) root cause of compiler failures

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 // src/App.tsx
-import React, { useState } from "react";
-import Login from "./Components/LoginSignUpPage/Login";
-import SignUp from "./Components/LoginSignUpPage/SignUp";
+import React from "react";
+// import React, { useState } from "react";
+// import Login from "./Components/LoginSignUpPage/Login";
+// import SignUp from "./Components/LoginSignUpPage/SignUp";
 import JournalPage from "./Components/JournalPage/Journal-Page";
 
 const App: React.FC = () => {
-  const [showSignUp, setShowSignUp] = useState(false);
+  // const [showSignUp, setShowSignUp] = useState(false);
 
   return (
     <JournalPage />
