7a3e14198bc1039fb47b412394e61e514db58bb6
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2024-11-20 14:19:55 -0800

    V4.0.1 resolve merge conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,18 @@
-// src/App.tsx
 import React, { useEffect, useState } from "react";
 import Login from "./Components/LoginSignUpPage/Login";
 import SignUp from "./Components/LoginSignUpPage/SignUp";
-//import DisplayUI from "./Components/DisplayUI/DisplayUI";
+import DisplayUI from "./Components/DisplayUI/DisplayUI";
 
 const App: React.FC = () => {
   // const [showSignUp, setShowSignUp] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isSigningUp, setIsSigningUp] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+	@@ -15,10 +15,11 @@ const App: React.FC = () => {
 
   return (
     <div>
-       
+        <DisplayUI/>
     </div>
   );
 };
 
 
 export default App;
-// <DisplayUI/>
